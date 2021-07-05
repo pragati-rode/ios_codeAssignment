21b58edd0c4c4144907f5b6cfb69510da64b80ed
Pragati Rode <pragatirode@Pragatis-MacBook-Air.local>
2021-07-05 14:36:01 +0530

    Initial Commit

---
+++ iOS_CodeAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_CodeAssignment
+//
+//  Created by Pragati Rode on 05/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
